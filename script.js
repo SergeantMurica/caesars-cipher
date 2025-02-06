6ffd1bc256dae0a3dac4bfe2f8ef895224e3b654
SergeantMurica <Iwontforgetureena@hotmail.com>
2025-02-05 20:09:19 -0700

    Formatting Fixes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function caesarCipher(string, shift) {
-    return string.replace(/[a-zA-Z]/g, function(c) {
+    return string.replace(/[a-zA-Z]/g, function (c) {
         const base = c < 'a' ? 65 : 97;
         return String.fromCharCode((c.charCodeAt(0) - base + shift) % 26 + base);
     });
